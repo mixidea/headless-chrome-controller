27495438be3b133cd03c55ec062d56d0ee7e6187
cloudliner <cloudliner.jp@gmail.com>
2018-07-28 22:22:59 +0900

    change url

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,18 @@ import express = require('express');
 import puppeteer = require('puppeteer');
 const app = express();
 
-app.use(async (req, res) => {
-  const url = req.query.url;
-  if (!url) {
+app.get('/recording/:event_id', async (req, res) => {
+  const event_id = req.params.event_id;
+  if (!event_id) {
     return res.send('Hello World!');
   }
+  let url = 'https://mixidea.org/headless-chrome/recording/';
+  if (app.locals.staging) {
+    url = 'https://staging.mixidea.org/headless-chrome/recording/';
+  } else if (app.locals.localhost) {
+    url = 'http://localhost:4200/recording/';
+  }
+  url = url + event_id;
   const browser = await puppeteer.launch({
     args: ['--no-sandbox']
   });
@@ -25,5 +32,12 @@ const server = app.listen(port, (error: any) => {
     return console.error(error);
   }
   console.info(`App listening on port ${port}`);
+  console.log(`NODE_LOCALHOST=${process.env.NODE_LOCALHOST === 'true'}`);
+  if (process.env.NODE_LOCALHOST === 'true') {
+    app.locals.localhost = true;
+  }
+  console.log(`NODE_STAGING=${process.env.NODE_STAGING === 'true'}`);
+  if (process.env.NODE_STAGING === 'true') {
+    app.locals.staging = true;
+  }
 });
-
