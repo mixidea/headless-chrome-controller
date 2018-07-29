eaa5e6c515fd97e4cb32aa9952684156c3cd67e3
cloudliner <cloudliner.jp@gmail.com>
2018-07-29 17:08:19 +0900

    change run setting

--- src/app.ts
+++ src/app.ts
@@ -14,13 +14,7 @@ app.get('/recording/:event_id', async (req, res) => {
         await res.end();
         return;
       }    
-      let url = 'https://mixidea.org/headless-chrome/recording/';
-      if (app.locals.staging) {
-        url = 'https://staging.mixidea.org/headless-chrome/recording/';
-      } else if (app.locals.localhost) {
-        url = 'http://localhost:4200/recording/';
-      }
-      url = url + event_id;
+      const url = app.locals.baseurl + event_id;
 
       await l.log('Launch Chrome', false);
       browser = await puppeteer.launch({
@@ -87,14 +81,15 @@ const server = app.listen(port, (error: any) => {
     return console.error(error);
   }
   console.info(`App listening on port ${port}`);
-  console.log(`NODE_LOCALHOST=${process.env.NODE_LOCALHOST === 'true'}`);
-  if (process.env.NODE_LOCALHOST === 'true') {
-    app.locals.localhost = true;
+  console.log(`NODE_TARGET=${ process.env.NODE_TARGET }`);
+  let baseurl = 'https://mixidea.org/headless-chrome/recording/';
+  if (process.env.NODE_TARGET === 'staging') {
+    baseurl = 'https://staging.mixidea.org/headless-chrome/recording/';
+  } else if (process.env.NODE_TARGET === 'localhost') {
+    baseurl = 'http://localhost:4200/recording/';
   }
-  console.log(`NODE_STAGING=${process.env.NODE_STAGING === 'true'}`);
-  if (process.env.NODE_STAGING === 'true') {
-    app.locals.staging = true;
-  }
+  app.locals.baseurl = baseurl;
+  console.log(`baseurl=${ app.locals.baseurl }`);
 });
 
 class Logger {
