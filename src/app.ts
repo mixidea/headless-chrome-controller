6a1bdc1c7d14030d25bc50d710d1673b6b3059e4
cloudliner <cloudliner.jp@gmail.com>
2018-07-29 20:59:42 +0900

    change image style

--- src/app.ts
+++ src/app.ts
@@ -107,7 +107,7 @@ class Logger {
     await this.res.write(`<p><span>${(Date.now() - this.startTime) / 1000}:</span> <span style="${style}">${message}</span></p>`);
     if (capture && this.page && ! this.page.isClosed() ) {
       const img = await this.page.screenshot({encoding: 'base64', fullPage: true});
-      await this.res.write(`<img src="data:image/png;base64,${img}">`);
+      await this.res.write(`<img src="data:image/png;base64,${img}" style="border-width:1px; border-style:dashed;">`);
     }
   }
   async error(message: any) {
