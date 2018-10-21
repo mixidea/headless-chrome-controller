7a051af21f02a20fefa2f9db45bf744cef792ad0
cloudliner <cloudliner.jp@gmail.com>
2018-10-21 19:18:02 +0900

    remove dumpio

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ async function launch_monitor_headlesschrome( req: any, res: any, l: Logger , ev
     await l.log(`!!!Launch Chrome!!! ${url} :  ${event_id}`, true);
     browser = await puppeteer.launch({
       args: ['--no-sandbox'],
-      dumpio: true
+      // dumpio: true
     });
     await l.log(`browser.version ${browser.version()}`);
     browser.on('targetdestroyed', () => console.log(`<<browser event>> targetdestroyed -  ${event_id}`));
