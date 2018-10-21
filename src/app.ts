d1fa1f8e880362be5b2df7d683d6b550c2fb7fd0
cloudliner <cloudliner.jp@gmail.com>
2018-10-21 18:32:39 +0900

    enable chrome log

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,8 @@ async function launch_monitor_headlesschrome( req: any, res: any, l: Logger , ev
 
     await l.log(`!!!Launch Chrome!!! ${url} :  ${event_id}`, true);
     browser = await puppeteer.launch({
-      args: ['--no-sandbox']
+      args: ['--no-sandbox'],
+      dumpio: true
     });
     browser.on('targetdestroyed', () => console.log(`<<browser event>> targetdestroyed -  ${event_id}`));
     browser.on('targetcreated', () => console.log(`<<browser event>> targetcreated -  ${event_id}`));
