84d8eda4b4b35de527ff96097c10d811937b4cf2
cloudliner <cloudliner.jp@gmail.com>
2018-12-24 20:33:31 +0900

    set timeout to 180000

--- src/record.ts
+++ src/record.ts
@@ -80,7 +80,7 @@ async function launch_monitor_headlesschrome(event_id: string, app_url: string, 
     page.on('close', () => console.log(`<<page event>> closed -  ${event_id}`));
     page.on('error', () => console.log(`<<page event>> error -  ${event_id}`));
 
-    await page.goto(url, { timeout: 120000 });
+    await page.goto(url, { timeout: 180000 });
     // await page.waitFor(1000);
     console.log(`!!!!!!!Open Page!!!!!!! -  ${event_id}`, true);
 
